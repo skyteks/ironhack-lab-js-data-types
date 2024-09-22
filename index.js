881873e46bfaa000aea8eeafd0147653493617d4
Skyteks <DanielEmig@gmx.de>
2024-09-22 22:25:11 +0200

    Solved lab

--- index.js
+++ index.js
@@ -8,10 +8,10 @@ const s4 = "bread";
 const s5 = "and";
 
 // Concatenate the string variables into one new string
-
+const result11 = `${s1} ${s2} ${s3} ${s4} ${s5}`;
 
 // Print out the concatenated string
-
+console.log(result11);
 
 
 
@@ -22,10 +22,12 @@ const part1 = "java";
 const part2 = "script";
 
 // Convert the last letter of part1 and part2 to uppercase and concatenate the strings
-
+part1[part1.length - 1].toUpperCase();
+part2[part2.length - 1].toUpperCase();
+const result12 = part1 + part2;
 
 // Print the cameLtaiL-formatted string
-
+console.log(result12)
 
 
 
@@ -35,10 +37,10 @@ const part2 = "script";
 const billTotal = 84;
 
 // Calculate the tip (15% of the bill total)
-
+const result21 = billTotal * 0.15;
 
 // Print out the tipAmount
-
+console.log(result21);
 
 
 
@@ -47,10 +49,10 @@ const billTotal = 84;
 *******************************************/
 
 // Generate a random integer between 1 and 10 (inclusive)
-
+const result22 = Math.random(10) + 1;
 
 // Print the generated random number
-
+console.log(result22);
 
 
 /*******************************************
@@ -61,16 +63,32 @@ const a = true;
 const b = false;
 
 // Try and guess the output of the below expressions first and write your answers down:
-const expression1 = a && b;
+const expression1 = a && b;//false
 
-const expression2 = a || b;
+const expression2 = a || b;//true
 
-const expression3 = !a && b;
+const expression3 = !a && b;//false
 
-const expression4 = !(a && b);
+const expression4 = !(a && b);//true
 
-const expression5 = !a || !b;
+const expression5 = !a || !b;//true
 
-const expression6 = !(a || b);
+const expression6 = !(a || b);//false
 
-const expression7 = a && a;
+const expression7 = a && a;//true
+
+const result31a = false;
+const result31b = true;
+const result31c = false;
+const result31d = true;
+const result31e = true;
+const result31f = false;
+const result31g = true;
+console.log(result31a == expression1,
+    result31b === expression2,
+    result31c === expression3,
+    result31d === expression4,
+    result31e === expression5,
+    result31f === expression6,
+    result31g === expression7
+)
